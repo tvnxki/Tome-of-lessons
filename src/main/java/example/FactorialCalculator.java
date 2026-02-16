4979f56fab2319ac7656c6d6fb0f78b03c3e23ac
tvnxki <143726295+tvnxki@users.noreply.github.com>
2026-02-16 16:31:21 +0300

    main logic hass added junit

--- src/main/java/example/FactorialCalculator.java
+++ src/main/java/example/FactorialCalculator.java
@@ -1,4 +1,17 @@
 package example;
 
 public class FactorialCalculator {
+
+    public long calculate(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал отрицательного числа не определен");
+        }
+
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+
+    }
 }
--- src/main/java/example/TriangleAreaCalculator.java
+++ src/main/java/example/TriangleAreaCalculator.java
@@ -1,4 +1,28 @@
 package example;
 
-public class TriangleCalculator {
+public class TriangleAreaCalculator {
+
+    public double calculateByThreeSides(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("Стороны треугольника должны быть положительными");
+        }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Треугольник с такими сторонами не существует");
+        }
+
+        double semiPerimeter = (a + b + c) / 2;
+        double area = Math.sqrt(semiPerimeter *
+                (semiPerimeter - a) *
+                (semiPerimeter - b) *
+                (semiPerimeter - c));
+        return Math.round(area * 100.0) / 100.0;
+    }
+
+    public double calculateByBaseAndHeight(double base, double height) {
+        if (base <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Основание и высота должны быть положительными");
+        }
+        double area = (base * height) / 2;
+        return Math.round(area * 100.0) / 100.0;
+    }
 }
